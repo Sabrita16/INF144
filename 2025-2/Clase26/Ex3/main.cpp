3b7b71e73e12aa2fd233dbfa7af76168012d42e0
Sabrita16 <s.lucio1602@gmail.com>
2025-11-24 18:06:12 -0500

    Hola

--- 2025-2/Clase26/Ex3/main.cpp
+++ 2025-2/Clase26/Ex3/main.cpp
@@ -7,5 +7,6 @@ int main() {
     imprimir_reporte_prueba_clientes(lista_clientes, "Reports/reporte_clientes_prueba_bloques.txt");
     leer_lista_libros(lista_libros, "Data/Libros.csv");
     imprimir_reporte_prueba_libros(lista_libros, "Reports/reporte_libros_prueba.txt");
+    //hola
     return 0;
 }
